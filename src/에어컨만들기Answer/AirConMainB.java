3c87e94e0a2a99cf516e9f3fdfc9539310ece0f8
JunHee <cyanaria89@gmail.com>
2026-03-16 14:49:35 +0900

    Human Education Center 26.03.16.

--- src/에어컨만들기Answer/AirConMainB.java
+++ src/에어컨만들기Answer/AirConMainB.java
@@ -2,18 +2,19 @@ package 에어컨만들기Answer;
 
 import java.util.Scanner;
 
-public class AirConMainA {
+public class AirConMainB {
     public static void main(String[] args) {
-        AirConA myAirCon = new AirConA();
+        AirConB myAirCon = new AirConB();
+        SmartAirConB mySmartAC = new SmartAirConB();
         Scanner sc = new Scanner(System.in);
 
         System.out.print("에어컨을 켜시겠습니까? (yes / no) : ");
         String onOff = sc.nextLine();
 
         if (onOff.equalsIgnoreCase("yes")) {
-            myAirCon.setPower(true);
-            myAirCon.setAirCon(sc);
-            myAirCon.operate();
+            mySmartAC.setPower(true);
+            mySmartAC.setAirCon(sc);
+            mySmartAC.operate();
         } else {
             System.out.println("에어컨을 켜지 않습니다.");
         }
